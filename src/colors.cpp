c417e5a5a547e9cc648532c29cc8a0cffdc1b3ba
zadnap <zadnap@users.noreply.github.com>
2025-02-24 10:13:06 +0700

    fix(colors): add const declaration to colors

--- src/colors.cpp
+++ src/colors.cpp
@@ -3,15 +3,15 @@
 #include <vector>
 using namespace std;
 
-Color TEXT_COLOR = {255, 255, 255, 255};
-Color BACKGROUND_COLOR = {38, 43, 57, 1};
-Color TETROMINO_I_COLOR = {0, 240, 240, 255};
-Color TETROMINO_O_COLOR = {240, 240, 0, 255};
-Color TETROMINO_T_COLOR = {160, 0, 240, 255};
-Color TETROMINO_L_COLOR = {240, 160, 0, 255};
-Color TETROMINO_J_COLOR = {0, 0, 240, 255};
-Color TETROMINO_S_COLOR = {0, 240, 0, 255};
-Color TETROMINO_Z_COLOR = {240, 0, 0, 255};
+const Color TEXT_COLOR = {255, 255, 255, 255};
+const Color BACKGROUND_COLOR = {38, 43, 57, 1};
+const Color TETROMINO_I_COLOR = {0, 240, 240, 255};
+const Color TETROMINO_O_COLOR = {240, 240, 0, 255};
+const Color TETROMINO_T_COLOR = {160, 0, 240, 255};
+const Color TETROMINO_L_COLOR = {240, 160, 0, 255};
+const Color TETROMINO_J_COLOR = {0, 0, 240, 255};
+const Color TETROMINO_S_COLOR = {0, 240, 0, 255};
+const Color TETROMINO_Z_COLOR = {240, 0, 0, 255};
 
 vector<Color> getCellColors()
 {
